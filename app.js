8e69f15413759b1943228549955ffecfb81eaecd
Anas khan <anaskhan41347@gmail.com>
2023-02-24 02:21:39 +0500

    Loops and Array

---
+++ app.js
@@ -0,0 +1,165 @@
+// Chapter (14 to 16)  // Array!
+
+
+
+// Questions Number 2 ;;;;;
+
+// var stdName = []
+
+
+
+// Questions Number 2 ;;;;;
+
+// var stdName = new Array[()];
+
+
+
+
+// Questions Number 3 ;;;;;
+
+// var string = ["ALI" , "HAMMAD" , "ASAD"];
+
+
+// Questions Number 4 ;;;;;
+// var numbers = [6 ,34 , 25]; 
+
+
+// Questions Number 5 ;;;;;
+
+// var boolean = [TRUE , FAlSE ]; 
+
+// Questions Number 6 ;;;;;
+// var mixedArray = ["SIMPLESTRING" , 299 , TRUE , UNDEFINED , NUL]; 
+
+// Questions Number 7 ;;;;;
+
+//  var educationStandandinpak = ["SSC" , "HSC" , "BCS" , "BS" , "BCOM" , "MS" , "M. Phil." , "PhD"];
+//  document.write("<h3> QUALIFICATIONS ;;;</h3>");
+//  for(i = 0 ; i <= educationStandandinpak.length ; i++){
+//      document.write("<h3>" + educationStandandinpak[i] + "</h3>")
+//  }
+
+
+
+// Questions Number8 ;;;;;
+
+// var Nameofstudents = ["MOHAMMAD" , "ANAS" , "KHAN"];
+// var Scores = [390 , 330 , 439];
+// var total = 500;
+// for(var i = 0; i < Nameofstudents.length; i++){
+//     var percentage = 100*(Scores[i]/total);
+//     document.write("<h3>" + "Scored of " + Nameofstudents[i] + " is " + Scores[i]+ "." + "Percentage:" + percentage + "%" + "</h3>" )
+// }
+
+// Questions Number 9 ;;;;;
+
+// var colorsName = ["Yellow" , "MARO" , "Blue"];
+// document.write(colorsName);
+
+// var ColorfromUsers = prompt("Please Confirm what Color Do you want to add to the beginning?");
+// colors.shift(ColorfromUsers);
+// document.write( ColorfromUsers);
+
+// var ColorfromUsers = prompt("Please Confirm what Color Do you want to add to the end?");
+// colors.push(ColorfromUsers);
+// document.write( ColorfromUsers);
+
+
+// colors.unshift("Purple" , "Black " ,"YELLOW" );
+// document.write( + colors);
+
+// / var Indexfromusers = prompt(" which Index Number do you want to add a color?");
+// var ColorIndex = prompt("which Color Name Wil you looking for add?");
+// colors.splice(Indexfromusers,0,ColorIndex);
+// document.write("<br/>" + colors);
+
+
+
+// Questions Number 10 ;;;;;
+
+// var allcities = ["Karachi" , "Lahore" , "Islamabad" , "Quetta" , "Peshawar"];
+// document.write("Cities list:"+ "<br/>" + allcities);
+// var selectedCitiesnames = allcities.splice(2,4); 
+// document.write(selectedCitiesnames);
+
+
+// Questions Number 11 ;;;;;
+
+// var ARRAY = ["This " , "IS " , "CAT " , "MY"];
+// document.write("<h2> Array:" + ARRAY + "</h3>")
+// var onlyString = ARRAY.join("");
+// document.write("<h3> String:" + "<br/>" + onlyString + "</h3>")
+
+Question #13
+
+// DEVICES.push("keyboard");
+// DEVICES.push("mouse");
+
+
+
+
+
+// Questions Number 12 ;;;;;
+
+// var phoneManufacturers = ["Apple" , "Sansung" , "Motorola" , "Nokia" , "Sony" , "Haier"];
+
+
+// for(var i = 0; i < phoneManufacturers.length; i++){
+    
+//     document.write("<option>" + phoneManufacturers[i] + "</option>");
+// }
+
+
+
+
+
+// Chapter (14 to 16)  // loops!
+// // Question #13
+// var emptyArray = []; 
+
+// // Question #14
+// var matrix = [
+//     [0 , 1 , 2 , 3],
+//     [1 , 0 , 1 , 2],
+//     [2 , 1 , 0 , 1]
+// ];
+
+// Questions Number 15 ;;;;;
+// var i = 0;
+// for(var i = 0  ; i <=10; i++){
+//     document.write(i + "<br/>");
+// }
+
+// // Question #16
+// Question #4
+
+// var alltableNumbers = +prompt("Enter Table Number");
+// var alltableLength = +prompt("Enter The Length of the Table");
+
+// document.write("<h3 > Multi of  " + alltableNumbers + "</h3>");
+// for(var i = 1; i <= table.Length; i++){
+//     document.write(alltableNumbers + " x " + i + " " + " = " + tableNumber * i + "<br/>");
+// }
+
+// Questions Number 17 ;;;;;
+
+// var fruits = ["Apple" , "Banana" , "Mango" , "Orange" , "Strawberry"];
+
+// for(i = 0; i <= 4 ; i++){
+//     document.write(fruits[i] + "<br/>");
+//     document.write("index elemet" + i + " is "  + fruits[i]);
+// }
+
+// Questions Number 18 ;;;;;
+
+// for(var i = 1 ; i <= 19; i++){
+//     document.write(" " + i);
+// }
+
+// for(var i = 19 ; i >= 1; i--){
+//     document.write(" " + i);
+
+
+// even
+// for(var i = 0 ; i <= 20; i+=2){
+//     document.write(" " + i);
